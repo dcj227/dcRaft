7da97ad2cedba0b81033056d00aff8aff6dd0178
breezeding <breezeding@tencent.com>
2019-04-08 15:24:08 +0800

    更新: 添加处理异步connect的逻辑.

--- include/core/socket_event.h
+++ include/core/socket_event.h
@@ -7,8 +7,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <queue>
 
 #define LISTENQUEUE 20
+#define CONNECT_TIMEOUT_MS 500
 
 namespace dc {
 
@@ -23,19 +25,54 @@ enum TCP_UDP {
     SOCKET_UDP
 };
 
+enum SOCKET_TYPE {
+    TYPE_LISTEN = 0,
+    TYPE_ACCEPT,
+    TYPE_CONNECT
+};
+
+enum SOCKET_STATE {
+    STATE_DEFAULT = 0,
+    STATE_LISTEN,
+    STATE_CONNECT,
+    STATE_READWRITE,
+    STATE_CLOSE
+};
+
+class SocketEvent;
+
 class SocketFdHandler {
 public:
+    SocketFdHandler(uint32_t ip,
+                    int port,
+                    SocketEvent* se,
+                    int fd = -1)     // 如果为Connection fd， 则可不填
+        : fd_(fd)
+        , ip_(ip)
+        , port_(port)
+        , se_(se) {
+    }
+
+    ~SocketFdHandler() {
+    }
+
     virtual void OnRecv(std::string& recveBuf) = 0;
     virtual void OnError(int fd, int err, std::string& error) = 0;
 
-    void Send(std::string& sendBuf) {
-        sendBuf_.append(sendBuf, 0, sendBuf.size());
-    };
+    void Send(std::string& sendBuf);
+
+    void SetFd(int fd);
 
     std::string& sendBuf() { return sendBuf_; };        // for SocketEvent use when OnWrite
 
 protected:
     std::string sendBuf_;
+
+    int fd_;
+    uint32_t ip_;
+    int port_;
+
+    SocketEvent* se_;
 };
 
 class SocketEvent : public EventHandler {
@@ -45,25 +82,38 @@ public:
 
     int Initialize();
 
-    virtual void OnRead(int fd);
-    virtual void OnWrite(int fd);
-    virtual void OnError(int fd, int err, std::string& error);
-    virtual void OnAccept(int fd, sockaddr_in& addr, socklen_t len) = 0;
+    virtual void OnRead(int fd, uint32_t events);
+    virtual void OnWrite(int fd, uint32_t events);
+    virtual void OnError(int fd, uint32_t events, int err, std::string& error);
 
-    int AddSocket(int fd, SocketFdHandler* sfd, uint32_t events = SOCKET_READ|SOCKET_WRITE|SOCKET_ERROR);
-    int AddListenSocket(std::string& ip_s, int port, SocketFdHandler* sfd, uint32_t events = SOCKET_READ|SOCKET_ERROR);
-    //int ModSocketEvent(int fd, uint32_t events);
+    virtual void OnAccept(int fd, uint32_t ip, int port) = 0;
+
+    /*
+     * delete sfd after DelSocket from SocketEvent
+     */
+    int AddListenSocket(std::string& ip_str, int port, uint32_t events = SOCKET_READ|SOCKET_ERROR);
+    int AddConnection(std::string& ip_str, int port, SocketFdHandler* sfd, uint32_t events = SOCKET_READ|SOCKET_ERROR);
     int DelSocket(int fd);
 
-    int Wait(int timeout);
+    int Wait(uint64_t now_ms, int timeout_ms = 0);
 
     SocketFdHandler* GetHandler(int fd);
 
+    int RemodSocketEvent(int fd);
+
     struct SocketInfo {
         int fd;
-        bool listen;
+        SOCKET_TYPE type;
+        SOCKET_STATE state;
         SocketFdHandler* handler; 
+        uint64_t expired_time_ms;       // 只有connect fd 会用到
     };
+
+protected:
+    /*
+     * delete sfd after DelSocket from SocketEvent
+     */
+    int AddSocket(int fd, SocketFdHandler* sfd, uint32_t events = SOCKET_READ|SOCKET_WRITE|SOCKET_ERROR);
 
 private:
     int SetNonBlocking(int fd);
@@ -76,6 +126,10 @@ private:
     std::map<int, SocketInfo> fd_si_;
 
     std::string recvBuf_;
+
+    uint64_t timeout_ms_;
+    uint64_t now_ms_;
+    std::queue<int> connect_fds_;
 };
 
 }   // namespace dc
--- src/core/socket_event.cpp
+++ src/core/socket_event.cpp
@@ -7,7 +7,19 @@
 
 namespace dc {
 
-SocketEvent::SocketEvent(TCP_UDP type/* = SOCKET_TCP*/, bool isEPOLLET/* = true*/)
+void SocketFdHandler::Send(std::string& sendBuf) {
+    sendBuf_.append(sendBuf, 0, sendBuf.size());
+    if (se_) {
+        se_->RemodSocketEvent(fd_);
+    } 
+};
+
+void SocketFdHandler::SetFd(int fd) {
+    fd_ = fd;
+}
+
+
+SocketEvent::SocketEvent(TCP_UDP type, bool isEPOLLET)
     : socket_type_(type)
     , epoll_event_(isEPOLLET) {
     recvBuf_.reserve(4096);
@@ -27,7 +39,7 @@ int SocketEvent::Initialize() {
     return 0;
 }
 
-void SocketEvent::OnRead(int fd) {
+void SocketEvent::OnRead(int fd, uint32_t events) {
     std::map<int, SocketInfo>::iterator it = fd_si_.find(fd);
     if (it == fd_si_.end()) {
         fprintf(stderr, "SocketEvent, fd OnRead but not in fd_si_ map, remove it, fd:%d\n", fd);
@@ -35,7 +47,7 @@ void SocketEvent::OnRead(int fd) {
         return;
     }
 
-    if (it->second.listen) {
+    if (it->second.type == TYPE_LISTEN) {
         sockaddr_in cli_addr;
         socklen_t cli_len;
         int cli_fd = accept(fd, (sockaddr *)&cli_addr, &cli_len);
@@ -43,9 +55,28 @@ void SocketEvent::OnRead(int fd) {
         if (cli_fd < 0) {
             fprintf(stderr, "SocketEvent, accept error, fd:%d, errno:%d, error:%s\n", fd, errno, strerror(errno));
         } else {
-            reinterpret_cast<SocketEvent*>(it->second.handler)->OnAccept(cli_fd, cli_addr, cli_len);
+            reinterpret_cast<SocketEvent*>(it->second.handler)->OnAccept(cli_fd, cli_addr.sin_addr.s_addr, cli_addr.sin_port);
         }
-        
+
+    } else if (it->second.type == TYPE_CONNECT && it->second.state == STATE_CONNECT) {  // 连接成功
+
+        int err = 0;
+        socklen_t len = sizeof(err);
+        int status = getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &len);
+        if (status == 0) {              // connect success
+            it->second.state = STATE_READWRITE;
+            OnRead(fd, SOCKET_READ);
+        } else {
+            std::string error = strerror(err);
+            it->second.handler->OnError(fd, err, error);
+        }
+
+        events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
+        int ret = epoll_event_.AddEvent(fd, this, events);
+        if (ret != 0) {
+            fprintf(stderr, "SocketEvent, fd OnRead AddEvent error, fd:%d, errno:%d, error:%s\n", fd, errno, strerror(errno));
+        }
+
     } else {
         int count = recv(fd, const_cast<char*>(recvBuf_.data()), recvBuf_.capacity(), MSG_PEEK);
         if (count < 0) {
@@ -53,7 +84,7 @@ void SocketEvent::OnRead(int fd) {
             // TODO
         } else if (count == 0) {
             DelSocket(it->second.fd); 
-        } else {
+        } else if (it->second.handler) {
             it->second.handler->OnRecv(recvBuf_);
         }
     }
@@ -61,12 +92,31 @@ void SocketEvent::OnRead(int fd) {
     return;
 }
 
-void SocketEvent::OnWrite(int fd) {
+void SocketEvent::OnWrite(int fd, uint32_t events) {
     std::map<int, SocketInfo>::iterator it = fd_si_.find(fd);
     if (it == fd_si_.end()) {
         fprintf(stderr, "SocketEvent, fd OnWrite but not in fd_si_ map, remove it, fd:%d\n", fd);
         epoll_event_.DelEvent(fd);
         return;
+    }
+
+    if (it->second.type == TYPE_CONNECT && it->second.state == STATE_CONNECT) {     // 连接成功
+
+        int err = 0;
+        socklen_t len = sizeof(err);
+        int status = getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &len);
+        if (status == 0) {              // connect success
+            it->second.state = STATE_READWRITE;
+        } else {
+            std::string error = strerror(err);
+            it->second.handler->OnError(fd, err, error);
+        }
+
+        events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
+        int ret = epoll_event_.AddEvent(fd, this, events);
+        if (ret != 0) {
+            fprintf(stderr, "SocketEvent, fd OnWrite AddEvent error, fd:%d, errno:%d, error:%s\n", fd, errno, strerror(errno));
+        }
     }
 
     SocketFdHandler* handler = it->second.handler;
@@ -80,18 +130,19 @@ void SocketEvent::OnWrite(int fd) {
             std::string leftBuf(sendBuf, count, sendBuf.size() - count);
             sendBuf.swap(leftBuf);
         } else {
-            sendBuf.empty();
+            //sendBuf.clear();
         }
     }
 }
 
-void SocketEvent::OnError(int fd, int err, std::string& error) {
+void SocketEvent::OnError(int fd, uint32_t events, int err, std::string& error) {
     std::map<int, SocketInfo>::iterator it = fd_si_.find(fd);
     if (it == fd_si_.end()) {
         fprintf(stderr, "SocketEvent, fd OnWrite but not in fd_si_ map, remove it, fd:%d\n", fd);
         epoll_event_.DelEvent(fd);
         return;
     }
+
     SocketFdHandler* handler = it->second.handler;
     if (handler) {
         handler->OnError(fd, err, error);
@@ -100,43 +151,26 @@ void SocketEvent::OnError(int fd, int err, std::string& error) {
 }
 
 /*
-void SocketEvent::OnAccept(int fd) {
+void SocketEvent::OnAccept(int fd, uint32_t ip, int port) {
     if (fd < 0) {
         fprintf(stderr, "SocketEvent, fd OnAccept fd < 0, fd:%d\n", fd);
         return;
     }
 
-    SocketFdHandler* phandler = new SocketFdHandler();
+    SocketFdHandler* phandler = new SocketFdHandler(fd, ip, port, this);
     
     int ret = AddSocket(fd, phandler);
     if (ret != 0) {
         fprintf(stderr, "SocketEvent, OnAccept, AddSocket fail, fd:%d, errno:%d, error:%s\n", fd, errno, strerror(errno));
-        return;
     }
 }
 */
 
-int SocketEvent::AddSocket(int fd, SocketFdHandler* sfd, uint32_t events/* = SOCKET_READ|SOCKET_WRITE|SOCKET_ERROR*/) {
-
-    std::map<int, SocketInfo>::iterator it = fd_si_.find(fd);
-    if (it != fd_si_.end()) {
-        return -1;
-    }
-
-    SetNonBlocking(fd);
-
-    SocketInfo si = {fd, false, sfd}; 
-    fd_si_[fd] = si; 
-
-    events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
-    return epoll_event_.AddEvent(fd, this, events);
-}
-
-int SocketEvent::AddListenSocket(std::string& ip_s, int port, SocketFdHandler* sfd, uint32_t events/* = SOCKET_READ|SOCKET_ERROR*/) {
+int SocketEvent::AddListenSocket(std::string& ip_str, int port, uint32_t events) {
     struct sockaddr_in addr;
     bzero(&addr, sizeof(addr));
     addr.sin_family = AF_INET;
-    inet_aton(ip_s.c_str(), &(addr.sin_addr)); 
+    inet_aton(ip_str.c_str(), &(addr.sin_addr)); 
     addr.sin_port=htons(port);
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -147,19 +181,64 @@ int SocketEvent::AddListenSocket(std::string& ip_s, int port, SocketFdHandler* s
 
     listen(fd, LISTENQUEUE);
 
-    SocketInfo si = {fd, true, sfd}; 
+    events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
+    int ret = epoll_event_.AddEvent(fd, this, events);
+    if (ret != 0) {
+        close(fd);
+        fprintf(stderr, "SocketEvent, AddEvent fail, ip:%s, port:%d, errno:%d, error:%s\n", ip_str.c_str(), port, errno, strerror(errno));
+    }
+
+    SocketInfo si = {fd, TYPE_LISTEN, STATE_LISTEN, NULL, 0}; 
     fd_si_[fd] = si; 
 
-    events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
-    return epoll_event_.AddEvent(fd, this, events);
+    return ret;
 }
 
-/*
-int SocketEvent::ModSocketEvent(int fd, uint32_t events) {
-    events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
-    return epoll_event_.ModEvent(fd, events); 
+int SocketEvent::AddConnection(std::string& ip_str, int port, SocketFdHandler* sfd, uint32_t events) {
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    inet_aton(ip_str.c_str(), &(addr.sin_addr)); 
+    addr.sin_port=htons(port);
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    SetNonBlocking(fd);
+
+    int ret = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+
+    if (ret == 0) {     // 连接成功, 本地连接可能出现
+        events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
+        int ret = epoll_event_.AddEvent(fd, this, events);
+        if (ret != 0) {
+            close(fd);
+
+            fprintf(stderr, "SocketEvent, AddConnection fail, ip:%s, port:%d, errno:%d, error:%s\n", ip_str.c_str(), port, errno, strerror(errno));
+            return -1;
+        }
+
+        sfd->SetFd(fd);
+         
+        SocketInfo si = {fd, TYPE_CONNECT, STATE_READWRITE, sfd, 0}; 
+        fd_si_[fd] = si; 
+
+        //sfd->OnRecv(recvBuf_);
+
+        return 0;
+    } else if (errno == EINPROGRESS) {      // 正在建立连接
+        sfd->SetFd(fd);
+
+        SocketInfo si = {fd, TYPE_CONNECT, STATE_CONNECT, sfd, now_ms_ + timeout_ms_ + CONNECT_TIMEOUT_MS};
+        fd_si_[fd] = si; 
+
+        connect_fds_.push(fd); 
+
+        return -2;
+    } else {
+        fprintf(stderr, "SocketEvent, AddConnection fail, ip:%s, port:%d, errno:%d, error:%s\n", ip_str.c_str(), port, errno, strerror(errno));
+        return -1;
+    }
 }
-*/
 
 int SocketEvent::DelSocket(int fd) {
     int ret = epoll_event_.DelEvent(fd);
@@ -174,8 +253,35 @@ int SocketEvent::DelSocket(int fd) {
     return 0;
 }
 
-int SocketEvent::Wait(int timeout) {
-    return epoll_event_.Wait(timeout);
+int SocketEvent::Wait(uint64_t now_ms, int timeout_ms) {
+    timeout_ms_ = timeout_ms;
+    now_ms_ = now_ms;
+
+    while (1) {
+        if (connect_fds_.size() <= 0) {
+            break;
+        }
+
+        int fd = connect_fds_.front();
+        std::map<int, SocketInfo>::iterator it = fd_si_.find(fd);
+        if (it == fd_si_.end()) {
+            connect_fds_.pop();
+            continue;
+        }
+
+        if (it->second.expired_time_ms < now_ms) {
+            if (it->second.handler) {
+                std::string error = "connect timeout.";
+                it->second.handler->OnError(fd, -2, error);
+            } 
+            fd_si_.erase(it);
+            connect_fds_.pop();
+            continue;
+        }
+        break;
+    };
+
+    return epoll_event_.Wait(timeout_ms);
 }
 
 SocketFdHandler* SocketEvent::GetHandler(int fd) {
@@ -185,6 +291,25 @@ SocketFdHandler* SocketEvent::GetHandler(int fd) {
     }
 
     return it->second.handler;
+}
+
+int SocketEvent::RemodSocketEvent(int fd) {
+    return epoll_event_.RemodEvent(fd); 
+}
+
+int SocketEvent::AddSocket(int fd, SocketFdHandler* sfd, uint32_t events) {
+    std::map<int, SocketInfo>::iterator it = fd_si_.find(fd);
+    if (it != fd_si_.end()) {
+        return -1;
+    }
+
+    SetNonBlocking(fd);
+
+    SocketInfo si = {fd, TYPE_ACCEPT, STATE_READWRITE, sfd, 0}; 
+    fd_si_[fd] = si; 
+
+    events = events & SOCKET_READ & SOCKET_WRITE & SOCKET_ERROR;
+    return epoll_event_.AddEvent(fd, this, events);
 }
 
 int SocketEvent::SetNonBlocking(int fd) {
